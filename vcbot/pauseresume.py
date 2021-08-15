b3d707f1d136f412004022ffe296032ee579f96e
Devesh Pal <newdev0@outlook.com>
2021-08-15 11:42:30 +0530

    Update and rename stopresume.py to pauseresume.py

---
+++ vcbot/pauseresume.py
@@ -0,0 +1,18 @@
+# Ultroid - UserBot
+# Copyright (C) 2021 TeamUltroid
+#
+# This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
+# PLease read the GNU Affero General Public License in
+# <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
+
+from . import *
+
+@vc_asst("pause")
+async def aoah(e):
+    CallsClient.pause_playout()
+    await eor(e, "∆ Paused.")
+
+@vc_asst("pause")
+async def aoah(e):
+    CallsClient.resume_playout()
+    await eor(e, "∆ Resumed.")
