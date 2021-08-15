765d1b944d59b4917766ac843706ffa817caf249
Devesh Pal <newdev0@outlook.com>
2021-08-15 11:36:52 +0530

    Update leavevc.py

--- vcbot/leavevc.py
+++ vcbot/leavevc.py
@@ -11,33 +11,3 @@ from . import *
 @vc_asst("stop")
 async def stop_vc(event):
     CallsClient.stop_playout()
-
-
-"""
-@asst.on_message(
-    filters.command(["leave", f"leave@{vcusername}"])
-    & filters.user(VC_AUTHS())
-    & ~filters.edited
-)
-async def leavehandler(_, message):
-    spli = message.text.split(" ", maxsplit=1)
-    try:
-        chat = (await Client.get_chat(spli[1])).id
-    except IndexError:
-        chat = message.chat.id
-    except Exception as Ex:
-        return await eor(message, str(Ex))
-    CallsClient.leave_group_call(chat)
-    m = await eor(message, "Successfully Left Group Call..")
-    try:
-        QUEUE.pop(chat)
-    except KeyError:
-        pass
-    await asyncio.sleep(5)
-    await m.delete()
-
-
-@Client.on_message(filters.me & filters.command("leavevc", HNDLR) & ~filters.edited)
-async def lhandler(_, message):
-    await leavehandler(_, message)
-"""
