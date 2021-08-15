c8d1cb6964ff36260fa1de490fad3a60e686a832
Devesh Pal <newdev0@outlook.com>
2021-08-15 11:37:57 +0530

    Update vchelp.py

--- vcbot/vchelp.py
+++ vcbot/vchelp.py
@@ -8,21 +8,7 @@
 
 HELP_TEXT = "**View All Vc Commands Here :**\nhttps://telegra.ph/Vc-Commands-07-17-2"
 
-"""
-@asst.on_message(
-    filters.command(["vchelp", f"vchelp@{vcusername}"])
-    & filters.user(VC_AUTHS())
-    & ~filters.edited
-    & filters.group
-)
-async def pass_it(_, message):
+
+@vc_cmd("vchelp")
+async def pass_it(message):
     await eor(message, HELP_TEXT)
-
-
-@Client.on_message(
-    filters.command("vchelp", HNDLR)
-    & filters.outgoing
-    & ~(filters.edited | filters.forwarded)
-)
-async def always(_, message):
-    await eor(message, HELP_TEXT)"""
